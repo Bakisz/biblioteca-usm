74cbb8c538e3f9428224c32747849ad585febbdc
Bakisz <martinmoragam@gmail.com>
2024-11-19 01:16:52 +0100

    cambios finales

--- src/Components/SecondNav.js
+++ src/Components/SecondNav.js
@@ -53,35 +53,27 @@ const SecondNav = () => {
                         className={styles.logoNegro}
                     />
                 </div>
+                <div className={`${styles.menuSection} ${menuOpen ? styles.menuOpen : ''}`}>
+                    <Link to="/" className={styles.navLink}>Inicio</Link>
+                    <div className={styles.dropdown}>
+                        <p className={styles.dropdownLink}>
+                            Apoyo a la investigación <span className={styles.smallArrow}>▼</span>
+                        </p>
+                        <div className={styles.dropdownMenu}>
+                            <Link to="/construccion" className={styles.dropdownItem}>Bases de Datos A-Z</Link>
+                            <Link to="/construccion" className={styles.dropdownItem}>Guías Temáticas</Link>
+                            <Link to="/construccion" className={styles.dropdownItem}>Recursos académicos abiertos</Link>
+                        </div>
+                    </div>
+                    <Link to="/construccion" className={styles.navLink}>Apoyo Memoristas</Link>
+                    <Link to="/construccion" className={styles.navLink}>ODS</Link>
+                    <Link to="/construccion" className={styles.navLink}>Quiénes somos</Link>
+                </div>
                 <button className={styles.hamburger} onClick={toggleMenu} aria-label="Abrir menú">
                     <span className={styles.line}></span>
                     <span className={styles.line}></span>
                     <span className={styles.line}></span>
                 </button>
-                {menuOpen && (
-                    <div className={styles.menuSection}>
-                        <Link to="/" className={styles.navLink} onClick={toggleMenu}>Inicio</Link>
-                        <div className={styles.dropdown}>
-                            <p className={styles.dropdownLink}>
-                                Apoyo a la investigación <span className={styles.smallArrow}>▼</span>
-                            </p>
-                            <div className={styles.dropdownMenu}>
-                                <Link to="/construccion" className={styles.dropdownItem} onClick={toggleMenu}>
-                                    Bases de Datos A-Z
-                                </Link>
-                                <Link to="/construccion" className={styles.dropdownItem} onClick={toggleMenu}>
-                                    Guías Temáticas
-                                </Link>
-                                <Link to="/construccion" className={styles.dropdownItem} onClick={toggleMenu}>
-                                    Recursos académicos abiertos
-                                </Link>
-                            </div>
-                        </div>
-                        <Link to="/Construccion" className={styles.navLink} onClick={toggleMenu}>Apoyo Memoristas</Link>
-                        <Link to="/Construccion" className={styles.navLink} onClick={toggleMenu}>ODS</Link>
-                        <Link to="/Construccion" className={styles.navLink} onClick={toggleMenu}>Quiénes somos</Link>
-                    </div>
-                )}
             </div>
             <div className={styles.navContainerLaptop2}></div>
         </>
